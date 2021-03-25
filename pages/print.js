e191d0b44862852642515df5e9c46bafaf5fc05e
Boris Grudinin <boris.grudinin@blinkpool.com>
2021-03-25 16:25:59 +0000

    fix image paths

--- pages/print.js
+++ pages/print.js
@@ -19,26 +19,27 @@ function Print() {
       <article className="page-content">
         <h2>Fianium</h2>
         <span className="date">2003 - 2016</span>
-        <img src="static/images/fianium/fianium-display3.jpg" alt="fianium backlit poster" />
+
+        <img src="../static/images/fianium/fianium-display3.jpg" alt="fianium backlit poster" />
         <p>Large, backlit tradeshow poster</p>
-        <img src="static/images/fianium/fianium-display4.jpg" alt="fianium display" />
+        <img src="../static/images/fianium/fianium-display4.jpg" alt="fianium display" />
         <p>Large, backlit tradeshow display stand</p>
-        <img src="static/images/fianium/fianium-displays.jpg" alt="fianium display design" />
+        <img src="../static/images/fianium/fianium-displays.jpg" alt="fianium display design" />
         <p>Various other tradeshow display stands</p>
-        <img src="static/images/fianium/fianium-logo-word-collage.png" alt="fianium logo word collage" />
+        <img src="../static/images/fianium/fianium-logo-word-collage.png" alt="fianium logo word collage" />
         <p>Stylised Fianium logo</p>
-        <img src="static/images/fianium/fianium-logo.jpg" alt="fianium logo" />
+        <img src="../static/images/fianium/fianium-logo.jpg" alt="fianium logo" />
         <p>Fianium logo and various print collateral</p>
-        <img src="static/images/fianium/fianium-product-photos.jpg" alt="fianium product photos" />
+        <img src="../static/images/fianium/fianium-product-photos.jpg" alt="fianium product photos" />
         <p>Product photography</p>
-        <img src="static/images/fianium/fianium-photos.jpg" alt="fianium photos" />
+        <img src="../static/images/fianium/fianium-photos.jpg" alt="fianium photos" />
         <p>General photography</p>
       </article>
 
       <article className="page-content">
         <h2>Various Projects</h2>
         <span className="date">2005 - Current</span>
-        <img src="static/images/print/nexus.jpg" />
+        <img src="../static/images/print/nexus.jpg" />
         <p>Logo for <a href="http://www.nexusmods.com" target="_blank">www.nexusmods.com</a></p>
       </article>
     </React.Fragment>
--- pages/web.js
+++ pages/web.js
@@ -18,11 +18,11 @@ function Web() {
       <article className="page-content">
         <h2>Get Fit with Davina</h2>
         <span className="date">2015 - 2016</span>
-        <img src="static/images/web/gfwd.jpg" />
+        <img src="../static/images/web/gfwd.jpg" />
         <p><strong>getfitwithdavina.com</strong> was a website designed to help people learn about nutrition and get in shape. I was involved with the wireframing, design and front end development.</p>
         <h2>Fianium</h2>
         <span className="date">2014 - 2016</span>
-        <a href="http://www.fianium.com" rel="nofollow" target="_blank"><img src="static/images/web/fianium.jpg" /></a>
+        <a href="http://www.fianium.com" rel="nofollow" target="_blank"><img src="../static/images/web/fianium.jpg" /></a>
         <p>Website for <a href="http://www.fianium.com" rel="nofollow" target="_blank">Fianium</a>, a laser manufacturer (now NKT Photonics). I was responsible for the design and implemention.</p>
       </article>
     </React.Fragment>
