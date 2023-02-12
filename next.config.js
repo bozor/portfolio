46aaf3d5a77abf4c4207fbe39baab67276d139cc
bozor <boris.grudinin@gmail.com>
2023-02-12 20:52:58 +0000

    update to latest version of next/react

--- next.config.js
+++ next.config.js
@@ -1,43 +1,16 @@
-const withPlugins = require('next-compose-plugins');
-const sass = require('@zeit/next-sass');
-const optimizedImages = require('next-optimized-images');
-
-const nextConfig = {
-  webpack: config => {
-    // Fixes npm packages that depend on `fs` module
-    config.node = {
-      fs: 'empty'
-    }
-
-    return config
-  },
-  exportPathMap: function () {
+module.exports = {
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
     return {
       '/': { page: '/' },
       '/web': { page: '/web' },
       '/print': { page: '/print' },
       '/apps': { page: '/apps' }
-    };
+    }
   },
   basePath: "/work",
   assetPrefix: "/work",
   trailingSlash: true,
 }
-
-module.exports = withPlugins([
-  [sass, {
-    sassLoaderOptions: {
-      sassOptions: {
-        includePaths: ["styles/*"],
-      }
-    }
-  }],
-  [optimizedImages, {
-    optimizeImagesInDev: true,
-    mozjpeg: {
-      quality: 85,
-    },
-    pngquant: true,
-    lqipLoader: true
-  }]
-], nextConfig);
