be683b3ed59f1266354b0ad159584c3f3ee701ed
Boris Grudinin <boris.grudinin@omm.agency>
2024-09-16 15:20:47 +0100

    add Image component with fade in when scrolled into viewport

--- app/apps/page.tsx
+++ app/apps/page.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import Image from 'next/image'
 import { Metadata, NextPage } from 'next'
 
 import Header from '@components/Header'
+import Image from '@components/Image'
 
 import ggArcade1 from '@images/apps/gg-arcade.jpg'
 import ggArcade2 from '@images/apps/gg-arcade2.jpg'
@@ -23,14 +23,14 @@ const Apps: NextPage = () => {
       <article className="page-content">
         <h2>GG Arcade <span>React Native, Azure Playfab</span></h2>
         <span className="date">2020</span>
-        <Image src={ggArcade1} alt='' width={0} height={0} />
-        <Image src={ggArcade2} alt='' width={0} height={0} />
-        <Image src={ggArcade3} alt='' width={0} height={0} />
+        <Image src={ggArcade1} alt='' />
+        <Image src={ggArcade2} alt='' />
+        <Image src={ggArcade3} alt='' />
         <p><strong>GG Arcade</strong> was a free to play social casino Android app with over 10k active users. Users could win tokens and exchange them for prizes which included game skins and gift vouchers for online stores like Amazon. I was responsible for the design along with the front end implementation.</p>
         <h2>PocketPredictor <span>React Native</span></h2>
         <span className="date">2019 - 2020</span>
-        <Image src={pp1} alt='' width={0} height={0} />
-        <Image src={pp2} alt='' width={0} height={0} />
+        <Image src={pp1} alt='' />
+        <Image src={pp2} alt='' />
         <p><strong>PocketPredictor</strong> was a free to play prediction Android app. Users could win tokens by correctly answering questions about future esports matches and exchange them for game skins. I was responsible for the initial prototype, design and the front end implementation.</p>
       </article>
     </>
