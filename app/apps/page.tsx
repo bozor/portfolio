e15604fa853c2fb36cb5a78299b51c39f47f475f
Boris Grudinin <boris.grudinin@omm.agency>
2025-09-17 22:26:46 +0100

    refactor apps and web pages to use new projects component and move content into an array of objects for better organisation

--- app/apps/page.tsx
+++ app/apps/page.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { Metadata, NextPage } from 'next';
 
 import Header from '@components/Header';
-import Image from '@components/Image';
+import Projects from '@components/Projects';
+
+import { TProject } from '@/src/types/project';
 
 import ggArcade1 from 'public/images/apps/gg-arcade.jpg';
 import ggArcade2 from 'public/images/apps/gg-arcade2.jpg';
@@ -13,35 +15,42 @@ import pp2 from 'public/images/apps/pp2.jpg';
 
 export const metadata: Metadata = { title: 'Apps' };
 
+const projects: TProject[] = [
+  {
+    title: 'GG Arcade',
+    technologies: 'React Native, Azure Playfab',
+    date: '2020',
+    imgSrcs: [ggArcade1, ggArcade2, ggArcade3],
+    description: [
+      <p>
+        <strong>GG Arcade</strong> was a free to play social casino Android app with over 10k active users. Users could win tokens and
+        exchange them for prizes which included game skins and gift vouchers for online stores like Amazon. I was responsible for the design
+        along with the front end implementation.
+      </p>
+    ]
+  },
+  {
+    title: 'PocketPredictor',
+    technologies: 'React Native',
+    date: '2019 - 2020',
+    imgSrcs: [pp1, pp2],
+    description: [
+      <p>
+        <strong>PocketPredictor</strong> was a free to play prediction Android app. Users could win tokens by correctly answering questions
+        about future esports matches and exchange them for game skins. I was responsible for the initial prototype, design and the front end
+        implementation.
+      </p>
+    ]
+  }
+];
+
 const Apps: NextPage = () => {
   return (
     <>
       <Header text="Apps" />
 
       <article className="page-content">
-        <h2>
-          GG Arcade <span>React Native, Azure Playfab</span>
-        </h2>
-        <span className="date">2020</span>
-        <Image src={ggArcade1} alt="" />
-        <Image src={ggArcade2} alt="" />
-        <Image src={ggArcade3} alt="" />
-        <p>
-          <strong>GG Arcade</strong> was a free to play social casino Android app with over 10k active users. Users could win tokens and
-          exchange them for prizes which included game skins and gift vouchers for online stores like Amazon. I was responsible for the
-          design along with the front end implementation.
-        </p>
-        <h2>
-          PocketPredictor <span>React Native</span>
-        </h2>
-        <span className="date">2019 - 2020</span>
-        <Image src={pp1} alt="" />
-        <Image src={pp2} alt="" />
-        <p>
-          <strong>PocketPredictor</strong> was a free to play prediction Android app. Users could win tokens by correctly answering
-          questions about future esports matches and exchange them for game skins. I was responsible for the initial prototype, design and
-          the front end implementation.
-        </p>
+        <Projects projects={projects} />
       </article>
     </>
   );
